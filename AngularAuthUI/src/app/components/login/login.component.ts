54dcd3f3204a15ea6c85c4862974dca8a19fa70b
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-10-24 10:00:24 -0500

    finished video 1, login/signup page

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+    this.isText ? this.type = 'text' : this.type = 'password';
   }
 
 }
